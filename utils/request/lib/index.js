647cbbaa81ac975408165611a71ed98c95960264
CVopen <1766730733@qq.com>
2022-02-22 22:33:30 +0800

    feat(version): version tip
    prepare & utils

--- utils/request/lib/index.js
+++ utils/request/lib/index.js
@@ -1,23 +1,6 @@
 'use strict'
 
-const axios = require('axios')
-
-const request = axios.create({
-  baseURL: process.env.CLI_BASE_URL || 'https://registry.npmjs.org',
-  timeout: 6000,
-})
-
-request.interceptors.response.use((response) => response.data)
-
-const get = ({ url, params, headers = {} }) => {
-  return request.request({
-    method: 'get',
-    url,
-    headers,
-    params,
-  })
-}
-
 module.exports = {
-  get,
+  ...require('./request'),
+  ...require('./npmapi'),
 }
--- utils/utils/lib/index.js
+++ utils/utils/lib/index.js
@@ -1,7 +1,19 @@
 'use strict'
 
-module.exports = utils
+const semver = require('semver')
 
-function utils() {
-  console.log('utils')
+function getLatestVersion(versions = [], currentVersion) {
+  if (!versions.length) {
+    return false
+  }
+
+  const laseVersion = versions.sort((a, b) => (semver.gt(b, a) ? 1 : -1))[0]
+  if (semver.eq(laseVersion, currentVersion)) {
+    return false
+  }
+  return laseVersion
+}
+
+module.exports = {
+  getLatestVersion,
 }
